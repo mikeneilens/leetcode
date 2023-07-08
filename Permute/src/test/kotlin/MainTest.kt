273471f0605c2a757c0cb9997750bacb0ddd0a64
Mike Neilens <mike.neilens@gmail.com>
2023-07-08 19:25:35 +0100

    Permute

--- Permute/src/test/kotlin/MainTest.kt
+++ Permute/src/test/kotlin/MainTest.kt
@@ -14,9 +14,13 @@ class MainTest: StringSpec ({
         permute(listOf(0,1)) shouldBe listOf(listOf(0,1),listOf(1,0))
     }
 
-    "[1,2,3] gives [[1,2],[2,1]]" {
+    "[1,2,3] gives [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]" {
         permute(listOf(1,2,3)) shouldBe
                 listOf(listOf(1,2,3),listOf(1,3,2),listOf(2,1,3),listOf(2,3,1),listOf(3,1,2),listOf(3,2,1))
+    }
+
+    "performance test [1,2,3,4,5,6,7] " {
+        permute(listOf(1,2,3,4,5,6,7))
     }
 
 })
