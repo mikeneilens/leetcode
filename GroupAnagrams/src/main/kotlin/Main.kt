fd44d286cb23a0f909c71e6a6c75212a3c0da69d
Mike Neilens <mike.neilens@gmail.com>
2023-07-16 17:20:43 +0100

    GroupAnagrams

--- GroupAnagrams/src/main/kotlin/Main.kt
+++ GroupAnagrams/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 
 fun groupAnagrams(strs: List<String>): List<List<String>> =
-    strs.groupBy { it.toList().sorted() }.toList().map{it.second.sorted()}
+    strs.groupBy (::charsInAlphabeticOrder).map(::valuesInEachGroupSorted)
 
+fun charsInAlphabeticOrder(str:String) = str.toList().sorted()
 
+fun valuesInEachGroupSorted(keyValue: Map.Entry<List<Char>, List<String>>) = keyValue.value.sorted()
